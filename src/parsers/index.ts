bde9b4defafd36fddce99e3eb6b33ef017d25f42
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-02-26 21:54:41 -0500

    Local FS only

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,15 +1,7 @@
 import Parser from "../struct/parser";
-import Batoto from "./batoto";
-import Madaradex from "./madaradex";
-import ToonilyParser from "./toonily";
-import Manga18FX from "./manga18fx";
 import LocalFS from "./localfs";
 
 const parserMapping: Map<string, Parser> = new Map<string, Parser>();
-parserMapping.set("batoto", new Batoto());
-parserMapping.set("madaradex", new Madaradex())
-parserMapping.set("toonily", new ToonilyParser())
-parserMapping.set("manga18fx", new Manga18FX())
 parserMapping.set("localfs", new LocalFS())
 
 export default parserMapping;
